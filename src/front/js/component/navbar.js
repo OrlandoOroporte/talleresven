40bce45c2fccb20ddb910c8dd5a853ab9aef6e09
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-09-13 23:04:39 +0000

    add navbar, lading page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo_taller from "../../img/Logo_taller2.jpg";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="container-fluid">
+      <nav className="navbar bg-light">
+        <Link className="navbar-brand" to="/">
+          <img
+            src={logo_taller}
+            alt="Logo"
+            width="50"
+            height="50"
+            className="d-inline-block align-text-top"
+          />
+          <h6>TalleresVenAPP</h6>
+        </Link>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">
+              Talleres
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Servicios
+            </Link>
+          </li>
+		  <li class="nav-item dropdown">
+          <Link class="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Usuarios
+          </Link>
+          <ul class="dropdown-menu">
+            <li><Link class="dropdown-item" to="/">Registro</Link></li>
+            <li><Link class="dropdown-item" to="/">Iniciar Sesión</Link></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><Link class="dropdown-item" to="/">Something else here</Link></li>
+          </ul>
+        </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,74 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div classNameName="container-fluid">
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="..."
+              className="d-block w-100 h-25"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="..."
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100 h-25" alt="..." />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+
+    // <div classNameName="text-center mt-5">
+    // 	<h1>Hello Rigo!!</h1>
+    // 	<p>
+    // 		<img src={rigoImageUrl} />
+    // 	</p>
+    // 	<div classNameName="alert alert-info">
+    // 		{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+    // 	</div>
+    // 	<p>
+    // 		This boilerplate comes with lots of documentation:{" "}
+    // 		<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+    // 			Read documentation
+    // 		</a>
+    // 	</p>
+    // </div>
+  );
 };
